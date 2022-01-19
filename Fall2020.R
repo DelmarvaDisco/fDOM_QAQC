ff034a402353a4beca9b95a2f100493a5a7cdd4f
James Maze <jtmaze@vt.edu>
2022-01-19 18:11:05 -0500

    - updated .gitignore and data folders

---
+++ Fall2020.R
@@ -0,0 +1,28 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Title: Fall 2020 fDOM processing
+#Coder: 
+#Date: 1/19/2022
+#Purpose: 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#Notes:
+
+#   - 
+#   -
+
+# 1. Libraries and work space ----------------------------------------------
+
+remove(list = ls())
+
+library(xts)
+library(dygraphs)
+library(purrr)
+library(lubridate)
+library(stringr)
+
+source("R/download_fun.R")
+
+# 2. Organize files and read data -----------------------------------------
+
+
+
--- R/download_fun.R
+++ R/download_fun.R
@@ -1,5 +1,17 @@
-#Title: Downloader
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Title: Downloading Exo files
 #Coder: James Maze (jtmaze@umd.edu)
 #Date: 1/19/2022
-#Purpose: To read in large batches of EXO downloads
+#Purpose: To read in large batches of EXO downloads and keep metadata
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+download_fun <- function(file_path){
+  #Read files
+  temp <- read_csv(paste0(file_path),
+                   col_names = TRUE, 
+                   skip = 1)
+  temp
+    
+}
+
 
